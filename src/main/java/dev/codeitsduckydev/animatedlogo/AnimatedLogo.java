941d7b8428f6943f45f512e0954fb94474e792dd
CodingWithDuckii <itsduckydev@gmail.com>
2026-07-29 16:10:43 +0300

    ???

--- src/main/java/dev/codeitsduckydev/animatedlogo/AnimatedLogo.java
+++ src/main/java/dev/codeitsduckydev/animatedlogo/AnimatedLogo.java
@@ -1,6 +1,9 @@
 package dev.codeitsduckydev.animatedlogo;
 
+import dev.codeitsduckydev.animatedlogo.gui.DevNoticeScreen;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
@@ -20,5 +23,14 @@ public class AnimatedLogo implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("Initializing Startup Animation");
         Registry.register(Registries.SOUND_EVENT, STARTUP_SOUND_ID, STARTUP_SOUND_EVENT);
+
+        // Show the developer notice every launch, the first time the title
+        // screen appears (i.e. after the boot animation finishes) -- unless
+        // the player has opted out via the "Don't show this again" checkbox.
+        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+            if (screen instanceof TitleScreen && !DevNoticeConfig.isDisabled()) {
+                client.setScreen(new DevNoticeScreen(screen));
+            }
+        });
     }
 }
